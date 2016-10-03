9250bdb4ceceb968a60f062a2d9f452ce84f1cc5
Jepp <user@user.com>
2016-10-03 22:31:23 +0700

    coba lagi 1

--- Review function no 13.cpp
+++ Review function no 13.cpp
@@ -3,6 +3,34 @@
  	
  	using namespace std;
  	
+ 	int noOfEmployee()
  	
+ 	{
+		int a;
+ 		do{
+		 
+		cout<<"Enter amount of employee: ";cin>>a;
+		} while(a < 1);
+		return a;
+	}
+
+
+	int missedDays(int emplo) 
+	
+	{
+		cout<<"Number of absence the employee did this past year."<<endl;
+		int noEmplo=1;
+		int totalAbsence=0, absence;
+		while(noEmplo<=emplo)
+		{
+			do{
+			cout<<"Employee no. "<<noEmplo<<" ";
+			cin>>absence;
+			} while (absence<0);
+			totalAbsence = totalAbsence + absence;
+			noEmplo++;
+		}
+		return totalAbsence;
+	}
 
  	
